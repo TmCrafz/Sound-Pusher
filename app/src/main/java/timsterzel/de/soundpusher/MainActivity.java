66ce2c871950124839421fdaa106f2edf05fc0ba
TmCrafz <sterzelTim@gmail.com>
2016-03-13 19:27:22 +0100

    Create simple record and play test

--- app/src/main/java/timsterzel/de/soundpusher/MainActivity.java
+++ app/src/main/java/timsterzel/de/soundpusher/MainActivity.java
@@ -1,23 +1,99 @@
 package timsterzel.de.soundpusher;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView m_recyclerView;
+    private RecyclerView.LayoutManager m_layoutManager;
+    private Button m_btnPlay;
+    private Button m_btnRecord;
+
+    private MediaRecorder m_recorder;
+    private MediaPlayer m_player;
+    private String m_recordPath;
+
+    private boolean m_recording;
+    private boolean m_playing;
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        }
+        else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        m_recorder = new MediaRecorder();
+        m_recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        m_recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        m_recorder.setOutputFile(m_recordPath + "/TestRecord.3gb");
+        m_recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            m_recorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "Record prepare failed: ", e);
+        }
+        m_recorder.start();
+        m_recording = true;
+    }
+
+    private void stopRecording() {
+        m_recorder.stop();
+        m_recorder.release();
+        m_recorder = null;
+        m_recording = false;
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        }
+        else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        m_player = new MediaPlayer();
+        try {
+            m_player.setDataSource(m_recordPath + "/TestRecord.3gb");
+            m_player.prepare();
+            m_player.start();
+        } catch (IOException e) {
+            Log.e(TAG, "Play prepare failed: ", e);
+        }
+        m_playing = true;
+    }
+
+    private void stopPlaying() {
+        m_player.release();
+        m_player = null;
+        m_playing = false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +102,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        mRecyclerView.setHasFixedSize(true);
+        m_recordPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File file = new File(m_recordPath);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                Log.e(TAG, "Error by creating file or directory: ", e);
+                e.printStackTrace();
+            }
+        }
 
-        mLayoutManager = new GridLayoutManager(this, 2);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        m_recording = false;
+        m_playing = false;
+
+        m_recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        m_btnPlay = (Button) findViewById(R.id.btnPlay);
+        m_btnRecord = (Button) findViewById(R.id.btnRecord);
+
+        m_btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRecord(!m_recording);
+                if (m_recording) {
+                    m_btnRecord.setText("Stop recording");
+                }
+                else {
+                    m_btnRecord.setText("Record");
+                }
+            }
+        });
+
+        m_btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPlay(!m_playing);
+                if (m_recording) {
+                    m_btnPlay.setText("Stop playing");
+                }
+                else {
+                    m_btnPlay.setText("Play");
+                }
+            }
+        });
+
+
+        m_recyclerView.setHasFixedSize(true);
+
+        m_layoutManager = new GridLayoutManager(this, 2);
+        m_recyclerView.setLayoutManager(m_layoutManager);
 
 
 
@@ -39,6 +160,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -66,4 +190,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
