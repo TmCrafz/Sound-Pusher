b4be806cb5b2892d75709d87d677ce833a2dea99
TmCrafz <sterzelTim@gmail.com>
2016-03-17 18:04:49 +0100

    Add missing char to legal chars

--- app/src/main/java/timsterzel/de/soundpusher/SoundFileHandler.java
+++ app/src/main/java/timsterzel/de/soundpusher/SoundFileHandler.java
@@ -27,7 +27,7 @@ public class SoundFileHandler {
     private final static char[] LEGAL_CHARS = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',
             'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-            'W', 'X', 'Y', '0', '1', '2', '3', '4', '5' , '6', '7', '8', '9' };
+            'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5' , '6', '7', '8', '9' };
 
 
     public static String moveFileToSoundPath(File file, String name) {
